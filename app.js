2c68b78bf8e91ff3f1e613bd93b98e63249b70c5
Ryan D. Lucas <rdlucas2@gmail.com>
2016-11-09 02:56:57 -0500

    add a store and share it across views and components

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
+//stores
+var weatherStore = {
+    state: {
+        location: 'Pittsburgh, PA',
+        weather: null
+    },
+    getWeather(location) {
+        location = encodeURIComponent(location);
+        var apiUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + location + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+        var xhr = new XMLHttpRequest();
+        var self = this;
+        xhr.open('GET', apiUrl);
+        xhr.onload = function() {
+            self.state.weather = JSON.parse(xhr.responseText);
+        }
+        xhr.send();
+    }
+}
+
 //views
 const Home = {
     template: '#home-template'
 }
+
 const About = {
-    template: '#about-template'
+    template: '#about-template',
+    data: function() {
+        return {
+            weatherStore: weatherStore
+        }
+    }
+
 }
 
 //routes
@@ -37,32 +63,13 @@ const Weather = Vue.component('weather-vue', {
     template: '#weather-template',
     data: function() {
         return {
-            currentWeather: null,
-            currentLocation: 'Pittsburgh, PA'
+            weatherStore: weatherStore
         }
     },
 
     created: function() {
-        this.fetchData(this.currentLocation);
-    },
-
-    watch: {
-        currentBranch: 'fetchData'
-    },
-
-    methods: {
-        fetchData: function(location) {
-            location = encodeURIComponent(location);
-            var apiUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + location + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-            var xhr = new XMLHttpRequest();
-            var self = this;
-            xhr.open('GET', apiUrl);
-            xhr.onload = function() {
-                self.currentWeather = JSON.parse(xhr.responseText);
-                console.log(self.currentWeather);
-            }
-            xhr.send();
-        }
+        if (this.weatherStore.state.weather) { return; }
+        this.weatherStore.getWeather(this.weatherStore.state.location);
     }
 });
 
